a1ff67ea89dc5a1cb48a4a1af39c2db644268770
krillo <krillo@boyhappy.se>
2010-08-31 17:57:18 +0200

    email field in db 200 chars

--- sql/change_user.sql
+++ sql/change_user.sql
@@ -3,3 +3,4 @@ alter table user drop block_id;
 alter table user drop column_id;
 alter table user drop order_id;
 alter table user add status int(11) NOT NULL default '0';
+alter table user change email `email` varchar(200) collate utf8_swedish_ci NOT NULL;
