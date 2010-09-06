107184e6d8b8419127e8a449c7f01d13ba1f5111
krillo <krillo@boyhappy.se>
2010-09-06 21:42:07 +0200

    new slicker layout on index page

--- htdocs/index.php
+++ htdocs/index.php
@@ -16,58 +16,60 @@ $maby = User::getStatusCount(User::MABY);
 ?>
 
 <div id="headnote">
-  <h1>Registrera dig för innebandy v34</h1>
+  <h1>Registrera dig för innebandy v<?php echo date('W')?></h1>
 </div>
 <div class="clear"></div>
 
 <div id="headnote-info">
-  <?php echo $attending; ?> kommer och <?php echo $maby; ?> är osäkra
+  <?php echo $attending; ?> kommer 
 </div>
 <div class="clear"></div>
 
-<table border="1">
+<table>
   <tr>
-    <th><a href="/players.php">Visitkort</a></th>
-    <th>X</th>
-    <th>Nej</th>
-    <th>?</th>
-    <th>Ja</th>
-     <th></th>
-    <th>Kommentar</th>
-    <th>Datum</th>
+    <th ><a href="/players.php">Visitkort</a></th>
+    <th >X</th>
+    <th >Nej</th>
+    <th >?</th>
+    <th >Ja</th>
+    <th ></th>
+    <th >Kommentar</th>
+    <th >Datum</th>
   </tr>
 
   <?php
   $users = new User;
   $result = $users->getUsers();
   while ($row = mysql_fetch_assoc($result)) {
+    $today = date('Ymd');
+    $date = date('Ymd', $row['unixdate']);
+    $today == $date ? $date = '<span style="color:#3764DF;">idag '. date('H:i', $row['unixdate']) .'</span>' : $date = date('j/n H:i', $row['unixdate']);
   ?>
     <form action="" method="post">
       <tr>
-        <td><a href="/players.php?id=<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a></td>
-        <td><input type="radio" name="status" value="0" <?php echo ($row['status'] == '0') ? 'checked' : ''; ?>/></td>
-        <td><input type="radio" name="status" value="1" <?php echo ($row['status'] == '1') ? 'checked' : ''; ?> /></td>
-        <td><input type="radio" name="status" value="2" <?php echo ($row['status'] == '2') ? 'checked' : ''; ?> /></td>
-        <td><input type="radio" name="status" value="3" <?php echo ($row['status'] == '3') ? 'checked' : ''; ?> /></td>
-        <td><input type="submit"  value=" Ok " /></td>
-        <td><input type="text"  name="comment" value="<?php echo $row['comment'] ?>"  class="comment-box" /></td>
-        <td><?php echo date('j/n H:i', $row['unixdate']); ?></td>
-      <input type="hidden" name="id" value="<?php echo $row['id'] ?>" />
+        <td class="list"><a href="/players.php?id=<?php echo $row['id'] ?>"><?php echo $row['name'] ?></a></td>
+        <td class="list"><input type="radio" name="status" value="0" <?php echo ($row['status'] == '0') ? 'checked' : ''; ?>/></td>
+        <td class="list"><input type="radio" name="status" value="1" <?php echo ($row['status'] == '1') ? 'checked' : ''; ?> /></td>
+        <td class="list"><input type="radio" name="status" value="2" <?php echo ($row['status'] == '2') ? 'checked' : ''; ?> /></td>
+        <td class="list"><input type="radio" name="status" value="3" <?php echo ($row['status'] == '3') ? 'checked' : ''; ?> /></td>
+        <td class="list"><input type="submit"  value=" Ok " /></td>
+        <td class="list"><input type="text"  name="comment" value="<?php echo $row['comment'] ?>"  class="comment-box" /></td>
+        <td class="list"><?php echo $date; ?></td>      
       </tr>
+      <input type="hidden" name="id" value="<?php echo $row['id'] ?>" />
     </form>
   <?php } ?>
   <!-- new user -->
   <form action="/edit_plyer.php" method="get">
     <tr>
-      <td><a href="/edit_player.php?id=new">Ny</a></td>
-      <td><input type="radio" name="status" value="0" disabled /></td>
-      <td><input type="radio" name="status" value="1" disabled /></td>
-      <td><input type="radio" name="status" value="2" disabled /></td>
-      <td><input type="radio" name="status" value="3" disabled /></td>
-      <td><input type="submit"  value=" Ok " disabled /></td>
-      <td><input type="text"  name="comment" value=""  class="comment-box" disabled /></td>
-     
-      <td></td>
+      <td ><a href="/edit_player.php?id=new">Ny</a></td>
+      <td ><input type="radio" name="status" value="0" disabled /></td>
+      <td ><input type="radio" name="status" value="1" disabled /></td>
+      <td ><input type="radio" name="status" value="2" disabled /></td>
+      <td ><input type="radio" name="status" value="3" disabled /></td>
+      <td ><input type="submit"  value=" Ok " disabled /></td>
+      <td ><input type="text"  name="comment" value=""  class="comment-box" disabled /></td>
+      <td > </td>
     </tr>
   </form>
 </table>
