e594afa6376c1450342bd9371cc1edb957abb119
krillo <krillo@boyhappy.se>
2011-05-17 19:26:34 +0200

    less framwork

---
+++ htdocs/less.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+	<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="author" content="boyhappy" />
+    <title>Registrera innebandy - less</title>
+
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+    <link rel="stylesheet" href="/css/lessframework.css"  type="text/css" media="all" />
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
+    <?php require_once $_SERVER['DOCUMENT_ROOT'].'/../init.php'; ?>
+	</head>
+
+	<body lang="en">
+		tjoho
+    
+	</body>
+
+</html>
